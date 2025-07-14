49002cd19f9ec96805e7c484e3c580242e751d1d
BloxClone <bloxclone.dev@gmail.com>
2025-07-14 11:41:09 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
-wget https://github.com/bloxclone/mybinder/raw/refs/heads/main/moddedb.zip
-unzip moddedb.zip
-cd modded
-chmod +x modded
-./modded
+# Check if mybinder directory exists
+if [ -d "mybinder" ] && [ -d "mybinder/modded" ]; then
+    echo "running directly..."
+    cd mybinder/modded
+    ./modded
+else
+    echo "downloading full..."
+    git clone https://github.com/bloxclone/mybinder
+    cd mybinder
+    tar -xvf modded-w.tar.gz
+    cd modded
+    chmod +x modded
+    ./modded
+fi
